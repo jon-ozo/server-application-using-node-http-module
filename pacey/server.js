d48ec766e06d8ce9e2d61bf95042060bfea231bb
John <johnozoemena@gmail.com>
2025-01-21 13:21:11 +0100

    added more routes to test the pacey framework

--- pacey/server.js
+++ pacey/server.js
@@ -1,5 +1,6 @@
 const Pacey = require('./pacey');
-const { join } = require('node:path');
+const { join, extname, format } = require('node:path');
+const { readdir } = require('node:fs/promises');
 
 const SESSIONS = [];
 
@@ -23,58 +24,34 @@ const POSTS = [
 		body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
 		userId: 2,
 	},
-	{
-		id: 3,
-		title: 'There are many variations of passages',
-		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-		userId: 3,
-	},
-	{
-		id: 4,
-		title: 'Contrary to popular belief',
-		body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-		userId: 4,
-	},
-	{
-		id: 5,
-		title: 'It was popularised in the 1960s',
-		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-		userId: 5,
-	},
-	{
-		id: 6,
-		title: 'It has roots in a piece of classical Latin',
-		body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-		userId: 5,
-	},
-	{
-		id: 7,
-		title: 'The generated Lorem Ipsum',
-		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-		userId: 4,
-	},
-	{
-		id: 8,
-		title: 'The point of using Lorem Ipsum',
-		body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-		userId: 3,
-	},
-	{
-		id: 9,
-		title: 'The standard chunk',
-		body: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
-		userId: 2,
-	},
-	{
-		id: 10,
-		title: 'Accompanied by English',
-		body: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
-		userId: 1,
-	},
 ];
 
 const PORT = 3000;
 const server = new Pacey();
+
+// server.middleware(async (req, res, next) => {
+// 	const mimeTypes = { '.js': 'text/javascript', '.css': 'text/css' };
+
+// 	try {
+// 		const folder = await readdir(join(__dirname, 'public'));
+
+// 		for await (const file of folder) {
+// 			const filePath = `/${file}`;
+// 			if (req.url === filePath) {
+// 				const fileFormat = extname(file);
+// 				const mimeType = Object.keys(mimeTypes).find(
+// 					(key) => key === fileFormat
+// 				);
+
+// 				return res.status(200).sendFile(filePath, mimeTypes[mimeType]);
+// 			}
+// 		}
+// 		next();
+// 	} catch (err) {
+// 		res.status(404).json({ error: err.message });
+// 		next();
+// 	}
+// });
 
 // run authentication
 server.middleware((req, res, next) => {
@@ -87,18 +64,17 @@ server.middleware((req, res, next) => {
 
 	if (routesToAuthenticate.indexOf(req.method + ' ' + req.url) !== -1) {
 		if (req.headers.cookie) {
-			const token = req.headers.cookie?.split('=')[1];
-			const checkTokenExists = SESSIONS.find(
-				(session) => session.token === token
-			);
+			const getToken = req.headers.cookie?.split('=')[1];
+			const token = SESSIONS.find((session) => session.token === getToken);
 
-			if (checkTokenExists) {
-				req.userId = checkTokenExists.userId;
+			if (token) {
+				req.userId = token.userId;
 				next();
 			}
+		} else {
+			res.status(401).json({ error: 'Unauthroized access denied' });
+			next();
 		}
-
-		return res.send(401).json({ error: 'Unauthroized access denied' });
 	} else {
 		next();
 	}
@@ -106,7 +82,7 @@ server.middleware((req, res, next) => {
 
 // parse json data
 server.middleware((req, res, next) => {
-	if ((req.headers['content-type'] = 'application/json')) {
+	if (req.headers['content-type'] === 'application/json') {
 		let body = '';
 
 		req.on('data', (chunk) => {
@@ -115,9 +91,8 @@ server.middleware((req, res, next) => {
 
 		req.on('end', () => {
 			req.body = JSON.parse(body);
+			next();
 		});
-
-		next();
 	} else {
 		next();
 	}
@@ -160,32 +135,84 @@ server.route('get', '/scripts.js', (req, res) => {
 // ------------------------------------------------------
 
 server.route('post', '/api/login', (req, res) => {
-	const { username, password } = req.body;
-	const user = USERS.find((user) => user.username === username);
+	try {
+		const { username, password } = req.body;
+		const user = USERS.find((user) => user.username === username);
 
-	if (user && user.password === password) {
-		const token = Math.floor(Math.random() * 100000000).toString();
-		const session = { userId: user.id, token };
+		if (user && user.password === password) {
+			const token = Math.floor(Math.random() * 100000000).toString();
+			const session = { userId: user.id, token };
 
-		SESSIONS.push(session);
+			SESSIONS.push(session);
 
-		res.setHeader('Set-Cookie', `token=${token}; Path=/`);
-		res.status(200).json({ message: 'Successfully logged in' });
-	} else {
-		res.status(401).json({ error: 'Invalid username or password' });
+			res.setHeader('Set-Cookie', `token=${token}; Path=/`);
+			res.status(200).json({ message: 'Successfully logged in' });
+			console.log(session);
+		} else {
+			res.status(401).json({ error: 'Invalid username or password' });
+		}
+	} catch (err) {
+		res.status(500).json({ error: err.message });
 	}
 });
 
-server.route('delete', '/api/logout', (req, res) => {});
+server.route('delete', '/api/logout', (req, res) => {
+	const index = SESSIONS.findIndex((session) => session.userId === req.userId);
 
-server.route('get', '/api/user', (req, res) => {
-	const user = USERS.find((user) => user.id === req.userId);
-	res.status(200).json({ username: user.username, name: user.name });
+	if (index === -1) return res.status(400).json({ error: 'Bad request' });
+
+	SESSIONS.splice(index, 1);
+
+	res.setHeader('Set-Cookie', `token=deleted; Path=/`);
+	res.status(200).json({ message: 'Successfully logged out' });
 });
 
-server.route('put', '/api/user', (req, res) => {});
+server.route('get', '/api/user', (req, res) => {
+	try {
+		const user = USERS.find((user) => user.id === req.userId);
 
-server.route('post', '/api/posts', (req, res) => {});
+		if (!user) return res.status(404).json({ error: 'User not found' });
+
+		res.status(200).json({ username: user.username, name: user.name });
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
+});
+
+server.route('put', '/api/user', (req, res) => {
+	try {
+		const { username, name, password } = req.body;
+		const user = USERS.find((user) => user.id === req.userId);
+
+		if (!user)
+			return res.status(401).json({ error: 'Unauthorized access denied' });
+
+		user.username = username;
+		user.name = name;
+
+		if (password) {
+			user.password = password;
+		}
+
+		res.status(200).json({ message: 'Profile saved successfully' });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+server.route('post', '/api/posts', (req, res) => {
+	const { title, body } = req.body;
+	const userId = req.userId;
+	const post = {
+		id: POSTS.length + 1,
+		title,
+		body,
+		userId,
+	};
+
+	POSTS.unshift(post);
+	console.log(POSTS);
+});
 
 server.route('get', '/api/posts', (req, res) => {
 	const posts = POSTS.map((post) => {
